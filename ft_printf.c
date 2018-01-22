10e83dd8c3f91701e1b17638d87939e8d68c0e74
Yevhenii BOHUSEVYCH <ybohusev@e2r7p9.unit.ua>
2018-01-22 17:34:21 +0200

    0.00000001

--- ft_printf.c
+++ ft_printf.c
@@ -12,7 +12,51 @@
 
 #include "ft_printf.h"
 
-int		ft_printf(const char *format, ...)
+static	char	*get_specifiers(char **format)
 {
-	
+	char	*type;
+	int		count;
+	char	*spec;
+	int		i;
+
+	type = "sSpdDioOuUxXcC";
+	count = 0;
+	while (*format[count])
+	{
+		i = 0;
+		while (type[i])
+		{
+			if (*format[count] == type[i])
+				break ;
+			i++;
+		}
+		count++;
+	}
+	spec = ft_strsub(*format, 1, count);
+	*format += count + 1;
+	return (spec);
+}
+
+static	void	parse(char **format, va_list ap)
+{
+	char	*specifiers;
+	char	*type;
+
+	specifiers = get_specifiers(format);
+}
+
+extern	int		ft_printf(const char *format, ...)
+{
+	va_list	ap;
+
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format == '%')
+			parse(%format);
+		else
+			ft_putchar(*format);
+		format++;
+	}
+
 }
